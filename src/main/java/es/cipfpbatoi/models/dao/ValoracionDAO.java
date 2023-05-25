e8b941405918a3e8c4501f735c20d0c43e011e54
Pablofasl <pablox2004x@gmail.com>
2023-05-25 17:18:50 +0200

    SQLValoracionDAO completado y clase de Valoracion creada

--- src/main/java/es/cipfpbatoi/models/dao/ValoracionDAO.java
+++ src/main/java/es/cipfpbatoi/models/dao/ValoracionDAO.java
@@ -1,11 +1,14 @@
 package es.cipfpbatoi.models.dao;
 
+import es.cipfpbatoi.exception.DatabaseErrorException;
 import es.cipfpbatoi.models.dto.Valoracion;
 import es.cipfpbatoi.models.dto.prods.Produccion;
 
 import java.util.ArrayList;
 
 public interface ValoracionDAO {
-    ArrayList<Valoracion> findAll();
-    void save(Valoracion valoracion);
+
+    ArrayList<Valoracion> findAll() throws DatabaseErrorException;
+
+    void save(Valoracion valoracion) throws DatabaseErrorException;
 }
