51b14fdac641b1a9900307c773d68182843ecdd1
Pablofasl <pablox2004x@gmail.com>
2023-05-25 16:29:25 +0200

    SQLProduccionDAO casi finalizada y 2 excepciones añadidas

--- src/main/java/es/cipfpbatoi/models/dto/prods/Produccion.java
+++ src/main/java/es/cipfpbatoi/models/dto/prods/Produccion.java
@@ -15,12 +15,12 @@ public class Produccion {
     private String productora;
     private String url_trailer;
     private String poster;
-    private Set<String> plataforam;
+    private Set<String> plataforma;
     private int visualizaciones;
     private String web;
     private Tipo tipo;
 
-    public Produccion(String id, String titulo, Calificacion calificacion, LocalDate fecha_lanzamiento, int duracion, int id_genero, Set<String> actores, String guion, String productora, String url_trailer, String poster, Set<String> plataforam, int visualizaciones, String web, Tipo tipo) {
+    public Produccion(String id, String titulo, Calificacion calificacion, LocalDate fecha_lanzamiento, int duracion, int id_genero, Set<String> actores, String guion, String productora, String url_trailer, String poster, Set<String> plataforma, int visualizaciones, String web, Tipo tipo) {
         this.id = id;
         this.titulo = titulo;
         this.calificacion = calificacion;
@@ -32,9 +32,69 @@ public class Produccion {
         this.productora = productora;
         this.url_trailer = url_trailer;
         this.poster = poster;
-        this.plataforam = plataforam;
+        this.plataforma = plataforma;
         this.visualizaciones = visualizaciones;
         this.web = web;
         this.tipo = tipo;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public Calificacion getCalificacion() {
+        return calificacion;
+    }
+
+    public LocalDate getFecha_lanzamiento() {
+        return fecha_lanzamiento;
+    }
+
+    public int getDuracion() {
+        return duracion;
+    }
+
+    public int getId_genero() {
+        return id_genero;
+    }
+
+    public Set<String> getActores() {
+        return actores;
+    }
+
+    public String getGuion() {
+        return guion;
+    }
+
+    public String getProductora() {
+        return productora;
+    }
+
+    public String getUrl_trailer() {
+        return url_trailer;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public Set<String> getPlataforma() {
+        return plataforma;
+    }
+
+    public int getVisualizaciones() {
+        return visualizaciones;
+    }
+
+    public String getWeb() {
+        return web;
+    }
+
+    public Tipo getTipo() {
+        return tipo;
     }
 }
