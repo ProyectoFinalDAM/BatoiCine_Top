47336f73efc3b62b27b165dfe195b49254588286
Andreu Francés Gallego <andreufgromeral@gmail.com>
2023-06-01 16:55:04 +0200

    TemporadaRepository

--- src/main/java/es/cipfpbatoi/models/respositories/TemporadaRepository.java
+++ src/main/java/es/cipfpbatoi/models/respositories/TemporadaRepository.java
@@ -1,5 +1,21 @@
 package es.cipfpbatoi.models.respositories;
 
-public class TemporadaRepository {
+import es.cipfpbatoi.exception.DatabaseErrorException;
+import es.cipfpbatoi.models.dao.TemporadaDAO;
+import es.cipfpbatoi.models.dto.prods.Temporada;
 
+import java.util.ArrayList;
+
+public class TemporadaRepository {
+    private TemporadaDAO temporadaDAO;
+
+    public TemporadaRepository(TemporadaDAO temporadaDAO) {
+        this.temporadaDAO = temporadaDAO;
+    }
+    ArrayList<Temporada> findAll() throws DatabaseErrorException{
+        return temporadaDAO.findAll();
+    }
+    void save(Temporada temporada) throws DatabaseErrorException{
+        temporadaDAO.save(temporada);
+    }
 }
