bc7435e786850c90a3481e8e608e9b2c41303825
Martín Peidro <peidromartin@gmail.com>
2023-06-02 15:37:36 +0200

    mostrar series y pelis

--- src/main/java/es/cipfpbatoi/controllers/SearchController.java
+++ src/main/java/es/cipfpbatoi/controllers/SearchController.java
@@ -111,7 +111,12 @@ public class SearchController implements Initializable {
         this.generoComboBox.setDisable(true);
         this.textFieldSearch.setEditable(false);
         this.portadaListView.setCellFactory((ListView<Produccion> p) -> new PosterPordController(valoracionRepository, rankingRepository, produccionRepository, this, "/views/search.fxml", user, visualizarRepository));
-        this.portadaListView.setPrefWidth(this.portadaListView.getFixedCellSize() * 4);
+
+        try {
+            showPage(portadaListView, hlAtras, hlSiguiente);
+        } catch (WrongParameterException ex) {
+            AlertCreator.infoAlert(ex.getMessage());
+        }
     }
 
     private ArrayList<Produccion> getCoincidencias(){
