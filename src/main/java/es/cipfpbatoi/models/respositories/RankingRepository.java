e1eaf78bf5a76bcbf52e3eef90c85ac7b14bd484
Martín Peidro <peidromartin@gmail.com>
2023-06-06 18:43:13 +0200

    Java doc terminado en los repositories

--- src/main/java/es/cipfpbatoi/models/respositories/RankingRepository.java
+++ src/main/java/es/cipfpbatoi/models/respositories/RankingRepository.java
@@ -6,6 +6,10 @@ import es.cipfpbatoi.models.dto.prods.Ranking;
 
 import java.util.ArrayList;
 
+/**
+ * Utiliza los métodos dao de para completar las clases necesarias para implementarlas mas tardes en los controller
+ */
+
 public class RankingRepository  {
     RankingDAO rankingDAO;
 
@@ -13,9 +17,23 @@ public class RankingRepository  {
         this.rankingDAO = rankingDAO;
     }
 
+    /**
+     * Busca todos los elementos del ranking
+     * @author Andreu Francés
+     * @return Una lista del resultado del ranking
+     * @throws DatabaseErrorException
+     */
+
     public ArrayList<Ranking> findAll() throws DatabaseErrorException {
         return rankingDAO.findAll();
     }
+
+    /**
+     * Guarda en la base de datos todos los datos sobre el ranking
+     * @author Andreu Francés
+     * @param ranking
+     * @throws DatabaseErrorException
+     */
 
     public void save(Ranking ranking) throws DatabaseErrorException {
         rankingDAO.save(ranking);
