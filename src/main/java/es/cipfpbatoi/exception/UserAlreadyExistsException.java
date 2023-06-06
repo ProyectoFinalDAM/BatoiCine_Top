a84b55809fc3094e7dd50af61764b2f3cee0514a
Martín Peidro <peidromartin@gmail.com>
2023-06-06 18:49:39 +0200

    Java doc terminado en las excepciones

--- src/main/java/es/cipfpbatoi/exception/UserAlreadyExistsException.java
+++ src/main/java/es/cipfpbatoi/exception/UserAlreadyExistsException.java
@@ -1,6 +1,12 @@
 package es.cipfpbatoi.exception;
 
 public class UserAlreadyExistsException extends Exception{
+
+
+    /**
+     * Muestra la excepción con el mensaje de usuario existente
+     * @author Marcos Sanz
+     */
     public UserAlreadyExistsException() {
         super("El usuario ya existe.");
     }
--- src/main/java/es/cipfpbatoi/exception/WrongParameterException.java
+++ src/main/java/es/cipfpbatoi/exception/WrongParameterException.java
@@ -1,6 +1,13 @@
 package es.cipfpbatoi.exception;
 
 public class WrongParameterException extends Exception{
+
+    /**
+     * Muestra la excepción con el mensaje pasado como paramaetro
+     * @author Martin Peidro
+     * @param message
+     */
+
     public WrongParameterException(String message) {
         super(message);
     }
